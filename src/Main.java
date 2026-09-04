06866ce950c31c139085f35cbfdea2cd19ff5bef
djy <djy0212092021@163.com>
2026-09-04 14:44:00 -0700

    first step

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        System.out.println("2 + 3 = " + calculator.add(2, 3));
+    }
+}
